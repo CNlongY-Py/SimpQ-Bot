8f789f887b5a4ca46a086583cddeab52bddd4df2
CNlongY-Py <79128694+CNlongY-Py@users.noreply.github.com>
2023-03-16 22:57:11 +0800

    输出控制台打印信息至log

--- start.py
+++ start.py
@@ -1,6 +1,8 @@
 import os
 import bot
-
+import time
 bot.initRun()  # 初始化运行
-for M in os.popen("python -m main"):  # 启动监听器
-    print(M.replace("\n", "")) # 输出
+with open("./logs/{}.log".format(time.strftime("%Y-%m-%d", time.localtime())),"a")as f:
+    for M in os.popen("python -m main"):  # 启动监听器
+        print(M.replace("\n", ""))
+        f.write(M)
