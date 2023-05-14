e47cafc0d47a981632d205ce6c9d5a74b76ebe0d
CNlongY-Py <79128694+CNlongY-Py@users.noreply.github.com>
2023-05-14 18:16:54 +0800

    RC2

--- start.py
+++ start.py
@@ -1,8 +1,8 @@
 import os
 import bot
 import time
+with open("./logs/%s.log" % time.strftime("%Y-%m-%d", time.localtime()), "a", encoding="utf-8") as f:
+    f.write("==============================\n")
 bot.initRun()  # 初始化运行
-with open("./logs/{}.log".format(time.strftime("%Y-%m-%d", time.localtime())),"a")as f:
-    for M in os.popen("python -m main"):  # 启动监听器
-        print(M.replace("\n", ""))
-        f.write(M)
+for M in os.popen("python -m main"):  # 启动监听器
+    print(M.replace("\n", ""))
